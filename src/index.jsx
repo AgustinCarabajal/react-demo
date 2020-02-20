e74c36bac8df2108e12e8d4b97d40f70e72114ea
Agus C <aguss360@gmail.com>
2020-02-20 17:02:56 -0300

    update sass

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { Switch, HashRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { persistor, store } from './store/Store';
 import ApplicationRoutes from './Routes/Routes';
 import AppRoute from './Routes/AppRoute';
@@ -35,7 +34,3 @@ ReactDOM.render(
   </Provider>, document.getElementById('root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
